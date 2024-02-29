07a073a8ec57767b29f1b2291238c85d2b093c28
IvyJebet <jebetivy388@gmail.com>
2024-02-29 08:14:08 -0800

    Balance factor Task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,14 +14,31 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
+	return (0);
+}
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Description: This function recursively calculates the height of a tree.
+ *              The height of a tree is defined as the number of edges on
+ *              the longest path from the root node to a leaf node.
+ *
+ * Return: If tree is NULL, the function returns 0.
+ * Otherwise, it returns the height.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 	if (tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
 	}
 	return (0);
